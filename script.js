89341ba5b6a844a4a67023feb9e35c3659de2ea1
HamadZafarullah <hamadzafarullah@gmail.com>
2022-10-24 16:27:21 -0400

    commit1

---
+++ script.js
@@ -0,0 +1,9 @@
+const getCard= document.querySelector("card")
+getCard.addEventListener("click", function clicked(event){
+    console.log("clicked")
+    console.log(event)
+})
+// function clicked(event){
+//     console.log("clicked")
+//     console.log(event)
+// 
