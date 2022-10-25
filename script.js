0dd6410fe6fea406dca12c62ca056dcdfd9c92b0
HamadZafarullah <hamadzafarullah@gmail.com>
2022-10-25 16:45:52 -0400

    working on flip

--- script.js
+++ script.js
@@ -1,9 +1,121 @@
-const getCard= document.querySelector("card")
-getCard.addEventListener("click", function clicked(event){
-    console.log("clicked")
-    console.log(event)
-})
-// function clicked(event){
+let getCard= document.querySelectorAll(".card")
+console.log(getCard)
+
+document.querySelector(".box").addEventListener("click",handleclick)
+function handleclick(evt) {
+    console.log(evt.target)
+   //event.target.className shows us what we clicked on 
+   // we can use that data to identifty if our user has clicked 
+   //on a card we can check by if the class name includes the card class 
+   //or equal to the card class once our user has clicked on a car we can 
+   //preform an action on it 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const getCard1= document.querySelector(".card1")
+// console.log(getCard1)
+// getCard1.addEventListener("click", (evt) => {
+//     var  evt= document.getElementsByClassName("card1");
 //     console.log("clicked")
-//     console.log(event)
-// 
+//     console.log(evt)
+// })
+// const getCard2= document.querySelector(".card2")
+// console.log(getCard2)
+// getCard2.addEventListener("click", (evt) => {
+//     var  evt= document.getElementsByClassName("card2");
+//     console.log("clicked")
+//     console.log(evt)
+// })
+// const getCard3= document.querySelector(".card3")
+// console.log(getCard3)
+// getCard3.addEventListener("click", (evt) => {
+//     var  evt= document.getElementsByClassName("card3");
+//     console.log("clicked")
+//     console.log(evt)
+// })
+// const getCard4= document.querySelector(".card4")
+// console.log(getCard4)
+// getCard4.addEventListener("click", (evt) => {
+//     var  evt= document.getElementsByClassName("card4");
+//     console.log("clicked")
+//     console.log(evt)
+// })
+// const getCard5= document.querySelector(".card5")
+// console.log(getCard5)
+// getCard5.addEventListener("click", (evt) => {
+//     var  evt= document.getElementsByClassName("card5");
+//     console.log("clicked")
+//     console.log(evt)
+// })
+// const getCard6= document.querySelector(".card6")
+// console.log(getCard6)
+// getCard6.addEventListener("click", (evt) => {
+//     var  evt= document.getElementsByClassName("card6");
+//     console.log("clicked")
+//     console.log(evt)
+// })
+// const getCard7= document.querySelector(".card7")
+// console.log(getCard7)
+// getCard7.addEventListener("click", (evt) => {
+//     var  evt= document.getElementsByClassName("card7");
+//     console.log("clicked")
+//     console.log(evt)
+// })
+// const getCard8= document.querySelector(".card8")
+// console.log(getCard8)
+// getCard8.addEventListener("click", (evt) => {
+//     var  evt= document.getElementsByClassName("card8");
+//     console.log("clicked")
+//     console.log(evt)
+// })
+// const getCard9= document.querySelector(".card9")
+// console.log(getCard9)
+// getCard9.addEventListener("click", (evt) => {
+//     var  evt= document.getElementsByClassName("card9");
+//     console.log("clicked")
+//     console.log(evt)
+// })
+// const getCard10= document.querySelector(".card10")
+// console.log(getCard10)
+// getCard10.addEventListener("click", (evt) => {
+//     var  evt= document.getElementsByClassName("card10");
+//     console.log("clicked")
+//     console.log(evt)
+// })
+
+
+
+  
